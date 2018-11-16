9d149b5005a73adfbb797ed7995a0e63f88dc854
Shibly Meeran <git@shib.me>
2018-11-16 20:17:07 +0530

    removing gmail from list adding the package status message

--- miui-debloat.sh
+++ miui-debloat.sh
@@ -1,61 +1,120 @@
+echo "Attempting to remove the package: com.android.egg"
 adb shell pm uninstall -k --user 0 com.android.egg
+echo "Attempting to remove the package: com.mi.android.globalpersonalassistant"
 adb shell pm uninstall -k --user 0 com.mi.android.globalpersonalassistant
+echo "Attempting to remove the package: com.xiaomi.mipicks"
 adb shell pm uninstall -k --user 0 com.xiaomi.mipicks
+echo "Attempting to remove the package: com.android.bookmarkprovider"
 adb shell pm uninstall -k --user 0 com.android.bookmarkprovider
+echo "Attempting to remove the package: com.android.browser"
 adb shell pm uninstall -k --user 0 com.android.browser
+echo "Attempting to remove the package: com.android.calendar"
 adb shell pm uninstall -k --user 0 com.android.calendar
+echo "Attempting to remove the package: com.miui.cleanmaster"
 adb shell pm uninstall -k --user 0 com.miui.cleanmaster
+echo "Attempting to remove the package: com.miui.translation.kingsoft"
 adb shell pm uninstall -k --user 0 com.miui.translation.kingsoft
+echo "Attempting to remove the package: com.miui.translation.youdao"
 adb shell pm uninstall -k --user 0 com.miui.translation.youdao
+echo "Attempting to remove the package: com.miui.translationservice"
 adb shell pm uninstall -k --user 0 com.miui.translationservice
+echo "Attempting to remove the package: com.miui.translation.youdao"
 adb shell pm uninstall -k --user 0 com.miui.translation.youdao
+echo "Attempting to remove the package: com.miui.translationservice"
 adb shell pm uninstall -k --user 0 com.miui.translationservice
+echo "Attempting to remove the package: com.miui.videoplayer.overlay"
 adb shell pm uninstall -k --user 0 com.miui.videoplayer.overlay
+echo "Attempting to remove the package: com.xiaomi.joyose"
 adb shell pm uninstall -k --user 0 com.xiaomi.joyose
+echo "Attempting to remove the package: com.mi.global.bbs"
 adb shell pm uninstall -k --user 0 com.mi.global.bbs
+echo "Attempting to remove the package: com.eterno"
 adb shell pm uninstall -k --user 0 com.eterno
+echo "Attempting to remove the package: com.android.bips"
 adb shell pm uninstall -k --user 0 com.android.bips
+echo "Attempting to remove the package: com.google.android.apps.docs"
 adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+echo "Attempting to remove the package: com.xiaomi.midrop"
 adb shell pm uninstall -k --user 0 com.xiaomi.midrop
+echo "Attempting to remove the package: com.google.android.apps.tachyon"
 adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+echo "Attempting to remove the package: com.microsoft.office.excel"
 adb shell pm uninstall -k --user 0 com.microsoft.office.excel
+echo "Attempting to remove the package: com.facebook.katana"
 adb shell pm uninstall -k --user 0 com.facebook.katana
+echo "Attempting to remove the package: com.facebook.system"
 adb shell pm uninstall -k --user 0 com.facebook.system
+echo "Attempting to remove the package: com.facebook.appmanager"
 adb shell pm uninstall -k --user 0 com.facebook.appmanager
+echo "Attempting to remove the package: com.facebook.services"
 adb shell pm uninstall -k --user 0 com.facebook.services
+echo "Attempting to remove the package: com.miui.bugreport"
 adb shell pm uninstall -k --user 0 com.miui.bugreport
+echo "Attempting to remove the package: com.miui.gallery"
 adb shell pm uninstall -k --user 0 com.miui.gallery
+echo "Attempting to remove the package: com.xiaomi.glgm"
 adb shell pm uninstall -k --user 0 com.xiaomi.glgm
-adb shell pm uninstall -k --user 0 com.google.android.gm
+echo "Attempting to remove the package: com.google.android.videos"
 adb shell pm uninstall -k --user 0 com.google.android.videos
+echo "Attempting to remove the package: com.google.android.music"
 adb shell pm uninstall -k --user 0 com.google.android.music
+echo "Attempting to remove the package: com.google.android.tts"
 adb shell pm uninstall -k --user 0 com.google.android.tts
+echo "Attempting to remove the package: com.xiaomi.payment"
 adb shell pm uninstall -k --user 0 com.xiaomi.payment
+echo "Attempting to remove the package: com.xiaomi.mirecycle"
 adb shell pm uninstall -k --user 0 com.xiaomi.mirecycle
+echo "Attempting to remove the package: com.miui.virtualsim"
 adb shell pm uninstall -k --user 0 com.miui.virtualsim
+echo "Attempting to remove the package: com.mi.global.shop"
 adb shell pm uninstall -k --user 0 com.mi.global.shop
+echo "Attempting to remove the package: com.mipay.wallet.in"
 adb shell pm uninstall -k --user 0 com.mipay.wallet.in
+echo "Attempting to remove the package: com.mi.webkit.core"
 adb shell pm uninstall -k --user 0 com.mi.webkit.core
+echo "Attempting to remove the package: com.miui.player"
 adb shell pm uninstall -k --user 0 com.miui.player
+echo "Attempting to remove the package: com.mi.globalTrendNews"
 adb shell pm uninstall -k --user 0 com.mi.globalTrendNews
+echo "Attempting to remove the package: com.microsoft.office.outlook"
 adb shell pm uninstall -k --user 0 com.microsoft.office.outlook
+echo "Attempting to remove the package: com.phonepe.app"
 adb shell pm uninstall -k --user 0 com.phonepe.app
+echo "Attempting to remove the package: com.microsoft.office.powerpoint"
 adb shell pm uninstall -k --user 0 com.microsoft.office.powerpoint
+echo "Attempting to remove the package: com.xiaomi.oversea.ecom"
 adb shell pm uninstall -k --user 0 com.xiaomi.oversea.ecom
+echo "Attempting to remove the package: com.skype.m2"
 adb shell pm uninstall -k --user 0 com.skype.m2
+echo "Attempting to remove the package: com.UCMobile.intl"
 adb shell pm uninstall -k --user 0 com.UCMobile.intl
+echo "Attempting to remove the package: com.miui.videoplayer"
 adb shell pm uninstall -k --user 0 com.miui.videoplayer
+echo "Attempting to remove the package: com.miui.vsimcore"
 adb shell pm uninstall -k --user 0 com.miui.vsimcore
+echo "Attempting to remove the package: com.miui.android.fashiongallery"
 adb shell pm uninstall -k --user 0 com.miui.android.fashiongallery
+echo "Attempting to remove the package: com.microsoft.office.word"
 adb shell pm uninstall -k --user 0 com.microsoft.office.word
+echo "Attempting to remove the package: com.miui.yellowpage"
 adb shell pm uninstall -k --user 0 com.miui.yellowpage
+echo "Attempting to remove the package: com.opera.mini.native"
 adb shell pm uninstall -k --user 0 com.opera.mini.native
+echo "Attempting to remove the package: com.til.timesnews"
 adb shell pm uninstall -k --user 0 com.til.timesnews
+echo "Attempting to remove the package: com.opera.app.news"
 adb shell pm uninstall -k --user 0 com.opera.app.news
+#echo 'Attempting to remove the package: com.kikaoem.xiaomi.qisiemoji.inputmethod'
 #adb shell pm uninstall -k --user 0 com.kikaoem.xiaomi.qisiemoji.inputmethod
+echo "Attempting to remove the package: com.miui.hybrid"
 adb shell pm uninstall -k --user 0 com.miui.hybrid
+echo "Attempting to remove the package: com.mfashiongallery.emag"
 adb shell pm uninstall -k --user 0 com.mfashiongallery.emag
+echo "Attempting to remove the package: com.mi.android.globalFileexplorer"
 adb shell pm uninstall -k --user 0 com.mi.android.globalFileexplorer
+echo "Attempting to remove the package: com.miui.notes"
 adb shell pm uninstall -k --user 0 com.miui.notes
+echo "Attempting to remove the package: com.opera.branding"
 adb shell pm uninstall -k --user 0 com.opera.branding
+echo "Attempting to remove the package: com.opera.branding.news"
 adb shell pm uninstall -k --user 0 com.opera.branding.news
